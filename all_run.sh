5cb903e594502da08b7bd3c1a591c9511dd6b5e6
ZAHEER AHMAD <zahmad@computefe2.rapidsilicon.local>
2023-02-07 14:31:53 +0500

    updated the flags options for local archtecture to all_run.sh

--- all_run.sh
+++ all_run.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# vpr_file="/nfs_scratch/scratch/CompilerValidation/zaheer_ahmad/bitstream_simulation_new/ArchBench_1/ArchBench/gemini_legacy_architecture/gemini_vpr.xml"
-# openfpga_file="/nfs_scratch/scratch/CompilerValidation/zaheer_ahmad/bitstream_simulation_new/ArchBench_1/ArchBench/gemini_legacy_architecture/gemini_openfpga.xml"
-# fixed_sim_openfpga_file="/nfs_scratch/scratch/CompilerValidation/zaheer_ahmad/bitstream_simulation_new/ArchBench_1/ArchBench/gemini_legacy_architecture/fixed_sim_openfpga.xml"
-# repack_design_constraint_file="/nfs_scratch/scratch/CompilerValidation/zaheer_ahmad/bitstream_simulation_new/ArchBench_1/ArchBench/gemini_legacy_architecture/repack_design_constraint.xml"
-# bitstream_annotation_file="/nfs_scratch/scratch/CompilerValidation/zaheer_ahmad/bitstream_simulation_new/ArchBench_1/ArchBench/gemini_legacy_architecture/bitstream_annotation.xml"
 set_device_size="castor10x8_heterogeneous"
 strategy="delay"
 main_path=$PWD
 files=$(find . -type f -iname "bit_sim.sh")
+default=0
 
 default_paths()
 {
 #    echo ""
+    default=1
     echo "default paths for architecture"
     vpr_file="../../../gemini_legacy_architecture/gemini_vpr.xml"
     openfpga_file="../../../gemini_legacy_architecture/gemini_openfpga.xml"
@@ -33,14 +30,12 @@ do
    esac
 done
 
-# Print helpFunction in case parameters are empty
 if [ -z "$vpr_file" ] || [ -z "$openfpga_file" ] || [ -z "$fixed_sim_openfpga_file" ] || [ -z "$repack_design_constraint_file" ] || [ -z "$bitstream_annotation_file" ]
 then
    echo "Some or all of the parameters are empty";
    default_paths
 fi
 
-# Begin script in case all parameters are correct
 echo "$vpr_file"
 echo "$openfpga_file"
 echo "$fixed_sim_openfpga_file"
@@ -50,7 +45,7 @@ echo "$bitstream_annotation_file"
 for i in $files; do
     directory_path=$(dirname $i) 
     cd $directory_path
-    bash bit_sim.sh $vpr_file $openfpga_file $fixed_sim_openfpga_file $repack_design_constraint_file $bitstream_annotation_file $set_device_size $strategy
+    bash bit_sim.sh $vpr_file $openfpga_file $fixed_sim_openfpga_file $repack_design_constraint_file $bitstream_annotation_file $set_device_size $strategy $default
     cd $main_path
 done
 
