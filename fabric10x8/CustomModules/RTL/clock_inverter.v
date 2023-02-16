84f2244ae6d184f80815d9cfcf652b0a81ae6625
ZAHEER AHMAD <zahmad@computefe2.rapidsilicon.local>
2023-02-16 12:55:20 +0500

    removed the gemini_legacy architecture and gemini_10x8 fabric from ArchBench

--- fabric10x8/CustomModules/RTL/clock_inverter.v
+++
@@ -1,6 +0,0 @@
-module clock_inverter (a,z);
-input a;
-output z;
-wire z;
-assign z = ~a;
-endmodule
--- fabric10x8/CustomModules/RTL/logic0.v
+++
@@ -1,5 +0,0 @@
-module logic0 (logic0);
-output logic0;
-wire logic0;
-assign logic0 = 1'b0;
-endmodule
