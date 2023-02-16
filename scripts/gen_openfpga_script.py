6fae9ed11351fd23117be9c6dee98a3967f9fdb9
ZAHEER AHMAD <zahmad@computefe2.rapidsilicon.local>
2023-02-16 12:45:07 +0500

    updated the script to generate fabric_netlist and bitstream.xml

--- scripts/gen_openfpga_script.py
+++ scripts/gen_openfpga_script.py
@@ -24,13 +24,61 @@ CURRENT_PATH=os.getcwd()
 # print("CURRENT_PATH",CURRENT_PATH)
 # ==========================================================
 
+# def update_custom_openfpga_script(design_dir,design_name, ftype='verilog', pcf=0, size='10x8',netlist='SRC'):
+#     netfile = "#"
+#     plfile = "#"
+#     rfile = "#"
+#     pcf_file_option = "#"
+#     design_input = ""
+#     file_content = ""
+#     crkt = "verilog"
+#     device = "castor10x8_heterogeneous"
+#     if netlist == 'SRC':
+#         fabric_key = os.getcwd() + "/10_8_updated_yaml_paths/output_fabric_key.xml"
+#     else:
+#         fabric_key = os.getcwd() + "/10_8_updated_yaml_paths/fabric_key.xml"
+#     if size == 'full':
+#         fabric_key = os.getcwd() + "/82_68_updated_yaml_paths/output_fabric_key.xml"
+#         device =  "castor82x68_heterogeneous"
+#     if(ftype == 'blif'):
+#         design_input = f"../{design_name}.blif"
+#         crkt = "blif"
+#     else:
+#         netfile = f"--net_file {design_name}_post_synth.net"
+#         plfile = f"--place_file {design_name}_post_synth.place"
+#         rfile = f"--route_file {design_name}_post_synth.route"
+#         design_input = f"{design_name}_post_synth.v"
+#     if pcf == 1:
+#         pcf_file_option = f"--fix_clusters ../{design_name}.place"
+#     if default==1:
+#         of = open("../../scripts/generate_testbench.openfpga",'r')
+#         openfpga_file= of.read()
+#         file_content = openfpga_file.format(vpr_file,design_input,pcf_file_option,device,netfile,plfile,rfile,crkt,openfpga_f,fixed_sim_openfpga_file,bitstream_annotation_file,repack_design_constraint_file,f"fabric_bitstream.xml",\
+#             f"fabric_bitstream.bit")
+#         file_content = file_content.replace("CURRENT_PATH",CURRENT_PATH+"/"+design_name+"_golden")
+#         if size == 'full':
+#             file_content = file_content.replace("gemini_10x8","gemini")
+#         of.close()
+#     else:
+#         of = open("../../scripts/openfpga_template_flags.openfpga",'r')
+#         openfpga_file= of.read()
+#         file_content = openfpga_file.format(vpr_file,design_input,pcf_file_option,device,netfile,plfile,rfile,crkt,openfpga_f,fixed_sim_openfpga_file,bitstream_annotation_file,repack_design_constraint_file,f"fabric_bitstream.xml",\
+#             f"fabric_bitstream.bit")
+#         if size == 'full':
+#             file_content = file_content.replace("gemini_10x8","gemini")
+#         of.close()
+#     design_openfpga_script = os.path.join(design_dir,f"{design_name}_custom.openfpga")
+#     dos = open(design_openfpga_script,'w+')
+#     dos.write(file_content)
+#     dos.close()
+#     return design_openfpga_script
+
 def update_custom_openfpga_script(design_dir,design_name, ftype='verilog', pcf=0, size='10x8',netlist='SRC'):
     netfile = "#"
     plfile = "#"
     rfile = "#"
     pcf_file_option = "#"
     design_input = ""
-    file_content = ""
     crkt = "verilog"
     device = "castor10x8_heterogeneous"
     if netlist == 'SRC':
@@ -50,23 +98,14 @@ def update_custom_openfpga_script(design_dir,design_name, ftype='verilog', pcf=0
         design_input = f"{design_name}_post_synth.v"
     if pcf == 1:
         pcf_file_option = f"--fix_clusters ../{design_name}.place"
-    if default==1:
-        of = open("../../scripts/generate_testbench.openfpga",'r')
-        openfpga_file= of.read()
-        file_content = openfpga_file.format(vpr_file,design_input,pcf_file_option,device,netfile,plfile,rfile,crkt,openfpga_f,fixed_sim_openfpga_file,bitstream_annotation_file,repack_design_constraint_file,f"fabric_bitstream.xml",\
-            f"fabric_bitstream.bit")
-        file_content = file_content.replace("CURRENT_PATH",CURRENT_PATH+"/"+design_name+"_golden")
-        if size == 'full':
-            file_content = file_content.replace("gemini_10x8","gemini")
-        of.close()
-    else:
-        of = open("../../scripts/openfpga_template_flags.openfpga",'r')
-        openfpga_file= of.read()
-        file_content = openfpga_file.format(vpr_file,design_input,pcf_file_option,device,netfile,plfile,rfile,crkt,openfpga_f,fixed_sim_openfpga_file,bitstream_annotation_file,repack_design_constraint_file,f"fabric_bitstream.xml",\
-            f"fabric_bitstream.bit")
-        if size == 'full':
-            file_content = file_content.replace("gemini_10x8","gemini")
-        of.close()
+    of = open("../../scripts/generate_testbench.openfpga",'r')
+    openfpga_file= of.read()
+    file_content = openfpga_file.format(design_input,pcf_file_option,device,netfile,plfile,rfile,crkt,"fabric_bitstream.xml",\
+        f"fabric_bitstream.bit")
+    file_content = file_content.replace("RAPTOR_PATH",RAPTOR_PATH)
+    if size == 'full':
+        file_content = file_content.replace("gemini_10x8","gemini")
+    of.close()
     design_openfpga_script = os.path.join(design_dir,f"{design_name}_custom.openfpga")
     dos = open(design_openfpga_script,'w+')
     dos.write(file_content)
