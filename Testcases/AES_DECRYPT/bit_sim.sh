620b407bef93307ac5d29aee9094b134b0a35fe9
ZAHEER AHMAD <zahmad@computefe2.rapidsilicon.local>
2023-02-22 16:03:52 +0500

    added the wrapper for AES_DECRYPT design

--- Testcases/AES_DECRYPT/bit_sim.sh
+++ Testcases/AES_DECRYPT/bit_sim.sh
@@ -56,7 +56,8 @@ echo "add_design_file ../rtl/decrypt.sv">>raptor.tcl
 echo "add_design_file ../rtl/generic_muxfx.v">>raptor.tcl
 echo "add_design_file ../rtl/gfmul.sv">>raptor.tcl
 echo "add_design_file ../rtl/wrapper.v">>raptor.tcl
-echo "set_top_module wrapper">>raptor.tcl
+echo "add_design_file ../rtl/wrapper_top.v">>raptor.tcl
+echo "set_top_module wrapper_top">>raptor.tcl
 echo "set_device_size $set_device_size">>raptor.tcl
 echo "add_constraint_file ../constrs_1/aes_decrypt.sdc">>raptor.tcl
 echo "custom_openfpga_script ../${design_name}_custom.openfpga">>raptor.tcl
