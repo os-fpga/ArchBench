498fe7eeaf01cba268461dcc4897396aa390727f
ZAHEER AHMAD <zahmad@computefe2.rapidsilicon.local>
2023-02-26 19:53:13 +0500

    updated the bitstream simulation testbench for and2

--- Testcases/and2/sim/bitstream_tb/and2_include_netlists.v
+++ Testcases/and2/sim/bitstream_tb/and2_include_netlists.v
@@ -11,5 +11,5 @@
 // ------ Include fabric top-level netlists -----
 `include "../../SRC/fabric_netlists.v"
 
-`include "../../sim/bitstream_tb/and2_top_formal_verification.v"
+// `include "../../sim/bitstream_tb/and2_top_formal_verification.v"
 `include "../../sim/bitstream_tb/and2_formal_random_top_tb.v"
