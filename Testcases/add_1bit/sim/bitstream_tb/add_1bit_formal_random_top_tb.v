9561f12ad7335e6b54f63548331640df813773cc
ZAHEER AHMAD <zahmad@computefe2.rapidsilicon.local>
2023-03-20 12:46:29 +0500

    updated the bitstream testbench for add_1bit

--- Testcases/add_1bit/sim/bitstream_tb/add_1bit_formal_random_top_tb.v
+++ Testcases/add_1bit/sim/bitstream_tb/add_1bit_formal_random_top_tb.v
@@ -3,7 +3,7 @@
 //	Description: FPGA Verilog Testbench for Formal Top-level netlist of Design: add_1bit
 //	Author: Xifan TANG
 //	Organization: University of Utah
-//	Date: Mon Feb 27 10:53:52 2023
+//	Date: Mon Mar 20 11:54:32 2023
 //-------------------------------------------
 //----- Time scale -----
 `timescale 1ns / 1ps
@@ -13,7 +13,7 @@
 
 module add_1bit_top_formal_verification_random_tb;
 // ----- Default clock port is added here since benchmark does not contain one -------
-	// reg [0:0] clk;
+	reg [0:0] clk;
 
 // ----- Shared inputs -------
 	reg [0:0] a;
@@ -24,15 +24,12 @@ module add_1bit_top_formal_verification_random_tb;
 
 // ----- FPGA fabric instanciation -------
 	// add_1bit_top_formal_verification FPGA_DUT(
-	// 	.a(a),
-	// 	.b(b),
-	// 	.c(c)
+	// 	a,
+	// 	b,
+	// 	c_gfpga
 	// );
 // ----- End FPGA Fabric Instanication -------
 
-	reg [0:3] clk;
-	wire [0:0] global_reset;
-	wire [0:0] scan_reset;
 	wire [0:639] gfpga_pad_QL_PREIO_A2F;
 	wire [0:639] gfpga_pad_QL_PREIO_F2A;
 	wire [0:639] gfpga_pad_QL_PREIO_F2A_DEF0;
@@ -46,28 +43,22 @@ module add_1bit_top_formal_verification_random_tb;
 	bit [0:0] config_enable;
 	wire [0:0] prog_clock;
 	bit [0:0] CFG_DONE;
-	
-	// ----- FPGA top-level module to be capsulated -----
-		fpga_top U0_formal_verification (
-			.clk(clk[0:3]),
-			.global_reset(global_reset[0]),
-			.scan_reset(scan_reset[0]),
-			.test_en(test_en[0]),
-			.scan_mode(scan_mode[0]),
-			.scan_clk(scan_clk[0]),
-			.config_enable(config_enable[0]),
-			.prog_clock(prog_clock[0]),
-			.CFG_DONE(CFG_DONE[0]),
-			.gfpga_pad_QL_PREIO_A2F(gfpga_pad_QL_PREIO_A2F[0:639]),
-			.gfpga_pad_QL_PREIO_F2A(gfpga_pad_QL_PREIO_F2A[0:639]),
-			.gfpga_pad_QL_PREIO_F2A_DEF0(gfpga_pad_QL_PREIO_F2A_DEF0[0:639]),
-			.gfpga_pad_QL_PREIO_F2A_DEF1(gfpga_pad_QL_PREIO_F2A_DEF1[0:639]),
-			.gfpga_pad_QL_PREIO_F2A_CLK(gfpga_pad_QL_PREIO_F2A_CLK[0:639]),
-			.ccff_head(ccff_head[0:9]),
-			.ccff_tail(ccff_tail[0:9]));
-	
-	// ----- Begin Connect Global ports of FPGA top module -----
-		// assign config_enable[0] = 1'b0;
+
+	fpga_top U0_formal_verification (
+		test_en[0],
+		scan_mode[0],
+		scan_clk[0],
+		config_enable[0],
+		prog_clock[0],
+		CFG_DONE[0],
+		gfpga_pad_QL_PREIO_A2F[0:639],
+		gfpga_pad_QL_PREIO_F2A[0:639],
+		gfpga_pad_QL_PREIO_F2A_DEF0[0:639],
+		gfpga_pad_QL_PREIO_F2A_DEF1[0:639],
+		gfpga_pad_QL_PREIO_F2A_CLK[0:639],
+		ccff_head[0:9],
+		ccff_tail[0:9]);
+
 		assign prog_clock[0] = 1'b0;
 		// assign CFG_DONE[0] = 1'b1;
 		assign test_en[0] = 1'b0;
@@ -77,95 +68,96 @@ module add_1bit_top_formal_verification_random_tb;
 		// assign clk[1] = 1'b0;
 		// assign clk[2] = 1'b0;
 		// assign clk[3] = 1'b0;
-		assign global_reset[0] = 1'b1;
-		assign scan_reset[0] = 1'b1;
+		// assign global_reset[0] = 1'b1;
+		// assign scan_reset[0] = 1'b1;
 
 // ----- Clock 'clk' Initialization -------
-	initial begin
-		clk[0] <= 1'b0;
-		clk[1] <= 1'b0;
-		clk[2] <= 1'b0;
-		clk[3] <= 1'b0;
-		while(1) begin
-			#1
-			clk[0] <= !clk[0];
-			clk[1] <= !clk[1];
-			clk[2] <= !clk[2];
-			clk[3] <= !clk[3];
+		initial begin
+			clk[0] <= 1'b0;
+			clk[1] <= 1'b0;
+			clk[2] <= 1'b0;
+			clk[3] <= 1'b0;
+			while(1) begin
+				#1
+				clk[0] <= !clk[0];
+				clk[1] <= !clk[1];
+				clk[2] <= !clk[2];
+				clk[3] <= !clk[3];
+			end
 		end
-	end
-	`include "../add_1bit/PinMapping.v"
+		`include "../add_1bit/PinMapping.v"
+
 // ----- Begin reset signal generation -----
 // ----- End reset signal generation -----
 
 // ----- Input Initialization -------
-	initial begin
-	  `include "../../bitstream_text.txt"
-	  config_enable[0]=1'b1;
-	  CFG_DONE=1'b1;
-	  #10;
+		initial begin
+			`include "../../bitstream_text.txt"
+			config_enable[0]=1'b1;
+			CFG_DONE=1'b1;
+			#10;
+			
+			a <= 1'b0;
+			b <= 1'b0;
+			#10
+			a = 1'b0;
+			b = 1'b0;
+			#15
+			if (c === 0)
+			  $display("Status: Test Passed");
+			else  
+			  $display("Status: Test Failed");
+			  
+			a = 1'b0; 
+			b = 1'b1;
+			#15
+			if (c === 1)
+			  $display("Status: Test Passed");
+			else  
+			  $display("Status: Test Failed");
+		  
+			a = 1'b1;
+			b = 1'b0;
+			#15
+			if (c === 1)
+			  $display("Status: Test Passed");
+			else  
+			  $display("Status: Test Failed");
 	  
-	  a <= 1'b0;
-	  b <= 1'b0;
-	  #10
-	  a = 1'b0;
-	  b = 1'b0;
-	  #15
-	  if (c === 0)
-		$display("Status: Test Passed");
-	  else  
-		$display("Status: Test Failed");
-		
-	  a = 1'b0; 
-	  b = 1'b1;
-	  #15
-	  if (c === 1)
-		$display("Status: Test Passed");
-	  else  
-		$display("Status: Test Failed");
-	
-	  a = 1'b1;
-	  b = 1'b0;
-	  #15
-	  if (c === 1)
-		$display("Status: Test Passed");
-	  else  
-		$display("Status: Test Failed");
-
-	  a = 1'b1;
-	  b = 1'b1;
-	  #15
-	  if (c === 0)
-		$display("Status: Test Passed");
-	  else  
-		$display("Status: Test Failed");
-	
-		a = 1'b0;
-		b = 1'b1;
-		#15
-		if (c === 1)
-		$display("Status: Test Passed");
-		else  
-		$display("Status: Test Failed");
-
-		a = 1'b1;
-		b = 1'b0;
-		#15
-		if (c === 1)
-		$display("Status: Test Passed");
-		else  
-		$display("Status: Test Failed");
-
-		a = 1'b1;
-		b = 1'b1;
-		#15
-		if (c === 0)
-		  $display("Status: Test Passed");
-		else  
-		  $display("Status: Test Failed");
-		#10
-		$finish;
-		end
+			a = 1'b1;
+			b = 1'b1;
+			#15
+			if (c === 0)
+			  $display("Status: Test Passed");
+			else  
+			  $display("Status: Test Failed");
+		  
+			  a = 1'b0;
+			  b = 1'b1;
+			  #15
+			  if (c === 1)
+			  $display("Status: Test Passed");
+			  else  
+			  $display("Status: Test Failed");
+	  
+			  a = 1'b1;
+			  b = 1'b0;
+			  #15
+			  if (c === 1)
+			  $display("Status: Test Passed");
+			  else  
+			  $display("Status: Test Failed");
+	  
+			  a = 1'b1;
+			  b = 1'b1;
+			  #15
+			  if (c === 0)
+				$display("Status: Test Passed");
+			  else  
+				$display("Status: Test Failed");
+			  #10
+			  $finish;
+	end
 
 // ----- Begin output waveform to VCD file-------
 	initial begin
--- Testcases/add_1bit/sim/bitstream_tb/add_1bit_include_netlists.v
+++ Testcases/add_1bit/sim/bitstream_tb/add_1bit_include_netlists.v
@@ -3,7 +3,7 @@
 //	Description: Netlist Summary
 //	Author: Xifan TANG
 //	Organization: University of Utah
-//	Date: Mon Feb 27 10:53:52 2023
+//	Date: Mon Mar 20 11:54:32 2023
 //-------------------------------------------
 //----- Time scale -----
 `timescale 1ns / 1ps
@@ -11,5 +11,5 @@
 // ------ Include fabric top-level netlists -----
 `include "../../../SRC/fabric_netlists.v"
 
-// `include "./SRC/add_1bit_top_formal_verification.v"
+// `include "BIT_SIM/add_1bit_top_formal_verification.v"
 `include "../../sim/bitstream_tb/add_1bit_formal_random_top_tb.v"
