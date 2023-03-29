8be66dde0998c749ab4ac58918a0251d3ad570cf
ZAHEER AHMAD <zahmad@computefe2.rapidsilicon.local>
2023-03-29 10:32:04 +0500

    updated the Device_name to device in parser and keyword.json

--- scripts/parser.py
+++ scripts/parser.py
@@ -213,7 +213,7 @@ def parse_log_files(files, log_line_keys_map):
                                 data[file][log_line_key] = 'Pass' if ' '.join(line.split(log_line_keyword)[1].split(" ")[i] for i in [2,3,4]).split('\n')[0] == "bitstream is generated" else 'Fail'
                             if log_line_key == 'Fabric_netlist_version':
                                 data[file][log_line_key] = line.split()[2]
-                            if log_line_key == 'Device_name':
+                            if log_line_key == 'device':
                                 data[file][log_line_key]=line.split()[-1]
                             
                 for line in lines[start_pb_usage+1:]:
