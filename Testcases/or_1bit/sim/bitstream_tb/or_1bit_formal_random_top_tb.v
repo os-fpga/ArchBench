7d52200a6e70f691f7abd53b8dc7aef98e46381e
zaheer.ahmad <zaheer.ahmad@rapidsilicon.com>
2023-04-18 10:33:18 +0500

    updated the bitstream simulation testbench for or_1bit

--- Testcases/or_1bit/sim/bitstream_tb/or_1bit_formal_random_top_tb.v
+++ Testcases/or_1bit/sim/bitstream_tb/or_1bit_formal_random_top_tb.v
@@ -3,127 +3,125 @@
 `default_nettype none
 
 module or_1bit_top_formal_verification_random_tb;
-	reg [0:0] a;
-	reg [0:0] b;
-	wire [0:0] c;
 
-	reg [0:3] clk;
-	wire [0:0] global_reset;
-	wire [0:0] scan_reset;
-	wire [0:639] gfpga_pad_QL_PREIO_A2F;
-	wire [0:639] gfpga_pad_QL_PREIO_F2A;
-	wire [0:639] gfpga_pad_QL_PREIO_F2A_DEF0;
-	wire [0:639] gfpga_pad_QL_PREIO_F2A_DEF1;
-	wire [0:639] gfpga_pad_QL_PREIO_F2A_CLK;
+	reg clock0;
+	reg a;
+	reg b;
+	wire c;
+
+	wire [0:7] clk;
+	wire [0:2303] gfpga_pad_QL_PREIO_A2F;
+	wire [0:2303] gfpga_pad_QL_PREIO_F2A;
+	wire [0:2303] gfpga_pad_QL_PREIO_F2A_DEF0;
+	wire [0:2303] gfpga_pad_QL_PREIO_F2A_DEF1;
+	wire [0:2303] gfpga_pad_QL_PREIO_F2A_CLK;
 	wire [0:9] ccff_head;
 	wire [0:9] ccff_tail;
-	wire [0:0] test_en;
-	wire [0:0] scan_mode;
-	wire [0:0] scan_clk;
-	bit [0:0] config_enable;
-	wire [0:0] prog_clock;
-	bit [0:0] CFG_DONE;
+	bit global_resetn;
+	wire scan_en;
+	wire scan_mode;
+	wire prog_clock;
 	
-		fpga_top U0_formal_verification (
-			.clk(clk[0:3]),
-			.global_reset(global_reset[0]),
-			.scan_reset(scan_reset[0]),
-			.test_en(test_en[0]),
-			.scan_mode(scan_mode[0]),
-			.scan_clk(scan_clk[0]),
-			.config_enable(config_enable[0]),
-			.prog_clock(prog_clock[0]),
-			.CFG_DONE(CFG_DONE[0]),
-			.gfpga_pad_QL_PREIO_A2F(gfpga_pad_QL_PREIO_A2F[0:639]),
-			.gfpga_pad_QL_PREIO_F2A(gfpga_pad_QL_PREIO_F2A[0:639]),
-			.gfpga_pad_QL_PREIO_F2A_DEF0(gfpga_pad_QL_PREIO_F2A_DEF0[0:639]),
-			.gfpga_pad_QL_PREIO_F2A_DEF1(gfpga_pad_QL_PREIO_F2A_DEF1[0:639]),
-			.gfpga_pad_QL_PREIO_F2A_CLK(gfpga_pad_QL_PREIO_F2A_CLK[0:639]),
-			.ccff_head(ccff_head[0:9]),
-			.ccff_tail(ccff_tail[0:9]));
+	fpga_top U0_formal_verification (
+		clk[0:7],
+		global_resetn,
+		scan_en,
+		scan_mode,
+		prog_clock,
+		gfpga_pad_QL_PREIO_A2F[0:2303],
+		gfpga_pad_QL_PREIO_F2A[0:2303],
+		gfpga_pad_QL_PREIO_F2A_DEF0[0:2303],
+		gfpga_pad_QL_PREIO_F2A_DEF1[0:2303],
+		gfpga_pad_QL_PREIO_F2A_CLK[0:2303],
+		ccff_head[0:9],
+		ccff_tail[0:9]);
 	
-		assign prog_clock[0] = 1'b0;
-		assign test_en[0] = 1'b0;
-		assign scan_mode[0] = 1'b0;
-		assign scan_clk[0] = 1'b0;
-		assign global_reset[0] = 1'b1;
-		assign scan_reset[0] = 1'b1;
+	assign prog_clock = 1'b0;
+	// assign global_resetn = 1'b0;
+	assign scan_en = 1'b0;
+	assign scan_mode = 1'b0;
+	assign clk[0] = clock0;
+	assign clk[1] = clock0;
+	assign clk[2] = clock0;
+	assign clk[3] = clock0;
+	assign clk[4] = clock0;
+	assign clk[5] = clock0;
+	assign clk[6] = clock0;
+	assign clk[7] = clock0;
 
-		initial begin
-			clk[0] <= 1'b0;
-			clk[1] <= 1'b0;
-			clk[2] <= 1'b0;
-			clk[3] <= 1'b0;
-			  while(1) begin
-				  #1
-				  clk[0] <= !clk[0];
-				  clk[1] <= !clk[1];
-				  clk[2] <= !clk[2];
-				  clk[3] <= !clk[3];
-			  end
-			end
-		  `include "../or_1bit/PinMapping.v"
+	initial begin
+		 clock0 <= 1'b0;
+		while(1) begin
+			#1
+			 clock0 <= !clock0;
+		end
+	end
+	`include "../or_1bit/PinMapping.v"
 
 	initial begin 
-	`include "../../bitstream_text.txt"
-	config_enable[0] = 1'b1;
-	CFG_DONE[0] = 1'b1;
-	a <= 1'b0;
-	b <= 1'b0;
+		`include "../../bitstream_text.txt"
+		// config_enable = 1'b1;
+		// CFG_DONE = 1'b1;
+		a <= 1'b0;
+		b <= 1'b0;
 
-      #5
-	  a = 1'b0;
-	  b = 1'b0;
-	  #5
-	  if (c === 0)
-		$display("Status: Test Passed");
-	  else  
-		$display("Status: Test Failed");
+		#5
+		global_resetn=1'b1;
+		// #5
+		if (c === 0)
+		  $display("Status: Test Passed");
+		else  
+		  $display("Status: Test Failed");
 		
-	  a = 1'b0; 
-	  b = 1'b1;
-	  #5
-	  if (c === 1)
-		$display("Status: Test Passed");
-	  else  
-		$display("Status: Test Failed");
+		a = 1'b0; 
+		b = 1'b1;
+		#5
+		if (c === 1)
+		  $display("Status: Test Passed");
+		else  
+		  $display("Status: Test Failed");
 
-	  a = 1'b1;
-	  b = 1'b0;
-	  #5
-	  if (c === 1)
-		$display("Status: Test Passed");
-	  else  
-		$display("Status: Test Failed");
+		a = 1'b1;
+		b = 1'b0;
+		#5
+		if (c === 1)
+		  $display("Status: Test Passed");
+		else  
+		  $display("Status: Test Failed");
 
-	  a = 1'b1;
-	  b = 1'b1;
-	  #5
-	  if (c === 1)
-		$display("Status: Test Passed");
-	  else  
-		$display("Status: Test Failed");
+		a = 1'b1;
+		b = 1'b1;
+		global_resetn=1'b0;
+		#5
+		if (c === 1)
+		  $display("Status: Test Passed");
+		else  
+		  $display("Status: Test Failed");
 
-	  a = 1'b0;
-	  b = 1'b1;
-	  #5
-	  if (c === 1)
-		$display("Status: Test Passed");
-	  else  
-		$display("Status: Test Failed");
+		a = 1'b0;
+		b = 1'b1;
+		#5
+		if (c === 1)
+		  $display("Status: Test Passed");
+		else  
+		  $display("Status: Test Failed");
 
-	  a = 1'b1;
-	  b = 1'b0;
-	  #5
-	  if (c === 1)
-		$display("Status: Test Passed");
-	  else  
-		$display("Status: Test Failed");
-	  #5
-	  $finish;
+		a = 1'b1;
+		b = 1'b0;
+		#5
+		if (c === 1)
+		  $display("Status: Test Passed");
+		else  
+		  $display("Status: Test Failed");
+		#5
+		$finish;
+		end
+
+	initial begin
+		$dumpfile("or_1bit.vcd");
+		$dumpvars(1, or_1bit_top_formal_verification_random_tb);
 	end
 
 endmodule
 
 `default_nettype none
-
--- Testcases/or_1bit/sim/bitstream_tb/or_1bit_include_netlists.v
+++ Testcases/or_1bit/sim/bitstream_tb/or_1bit_include_netlists.v
@@ -1,15 +1,6 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: Netlist Summary
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//	Date: Sun Feb 26 20:26:03 2023
-//-------------------------------------------
-//----- Time scale -----
 `timescale 1ns / 1ps
 
-// ------ Include fabric top-level netlists -----
 `include "../../../SRC/fabric_netlists.v"
 
-// `include "../../sim/bitstream_tb/or_1bit_top_formal_verification.v"
+// `include "SRC/or_1bit_top_formal_verification.v"
 `include "../../sim/bitstream_tb/or_1bit_formal_random_top_tb.v"
