e3dcc0d63acb5dbf340bc36ceb870c6e4994ae6e
zaheer.ahmad <zaheer.ahmad@rapidsilicon.com>
2023-04-26 17:13:09 +0500

    removed the latches from dec and fft_fir_filter designs that are failing due to latches

--- Testcases/fft_fir_filter/rtl/ramb16_s36_s36.vhd
+++ Testcases/fft_fir_filter/rtl/ramb16_s36_s36.vhd
@@ -70,53 +70,58 @@ architecture BEH of RAMB16_S36_S36 is
 	type mem is array (0 to 511) of std_logic_vector (35 downto 0);							  
 	signal adra,adrb:std_logic_vector (8 downto 0):=(others=>'0');	   						  
 	signal data_a,data_b:std_logic_vector (35 downto 0);
-	signal wea_1,web_1: STD_LOGIC; 
+	signal wea_1,web_1: STD_LOGIC;
+	signal ram: mem:=(others=>"000000000000000000000000000000000000") ; 
 begin  
-	process(clka,ssra,clkb,ssrb,adra,adrb,wea,web,dia,dib)  
-		variable ram: mem:=(others=>X"000000000") ;	
-		variable ia,ib: integer;	   
-		variable a,b:std_logic_vector (35 downto 0);
+	process(clka,ssra,clkb,ssrb,adra,adrb,wea,web,dia,dib,wea_1,data_a,web_1,data_b,ram)  
+	--variable ram: mem:=(others=>"000000000000000000") ;	
+	variable ia,ib: integer;	   
+	variable a,b:std_logic_vector (35 downto 0);
 	begin
-		
-		if ssra = '1' then 
-			dopa<="0000";
-			doa <= X"00000000";	 
-			adra <= "000000000";			
-			wea_1 <= '0';   
-		elsif clka = '1' and clka'event then 
-			data_a <= dipa&dia;	
-			adra(8 downto 0) <= addra;
-			wea_1 <= wea;
-		end if;	
-		
-		if ssrb = '1' then 
-			dopb<="0000";
-			dob <= X"00000000";	 	 
-			adrb <= "000000000"; 			
-			web_1 <= '0';
-		elsif clkb = '1' and clkb'event then 
-			data_b <= dipb&dib;	
-			adrb(8 downto 0)  <= addrb; 			
-			web_1 <= web;
+
+
+	ia:= conv_integer(To_X01(adra));		                 
+	a:=	 ram(ia); 
+	dopa<=a(35 downto 32);
+	doa <= a(31 downto 0); 
+	if clka = '1' and clka'event then
+		if wea = '1' then
+			ram(ia) <=data_a;           
 		end if;
-		
-		ia:= conv_integer(To_X01(adra));		                 
-		a:=	 ram(ia); 
-		dopa<=a(35 downto 32);
-		doa <= a(31 downto 0); 
-		if wea_1 = '1' then
-			ram(ia) :=data_a;           
-		end if;	 	
-		
-		
-		ib:= conv_integer(To_X01(adrb));  		                 
-		b:=	 ram(ib); 
-		dopb<=b(35 downto 32);
-		dob <= b(31 downto 0);  
-		if web_1 = '1' then
-			ram(ib) := data_b;           
+	end if;	 	
+
+
+	ib:= conv_integer(To_X01(adrb));  		                 
+	b:=	 ram(ib); 
+	dopb<=b(35 downto 32);
+	dob <= b(31 downto 0); 
+	if clka = '1' and clka'event then 
+		if web = '1' then
+			ram(ib) <= data_b;           
 		end if;
-		
-	end process;
+	end if;
+
+	if ssra = '1' then 
+		dopa<="0000";
+		doa <= X"00000000";	 
+		adra <= "000000000";			
+		wea_1 <= '0';   
+	elsif clka = '1' and clka'event then 
+		data_a <= dipa&dia;	
+		adra(8 downto 0) <= addra;
+		wea_1 <= wea;
+	end if;	
+
+	if ssrb = '1' then 
+		dopb<="0000";
+		dob <= X"00000000";	 	 
+		adrb <= "000000000";			
+		web_1 <= '0';
+	elsif clkb = '1' and clkb'event then 
+		data_b <= dipb&dib;	
+		adrb(8 downto 0)  <= addrb; 			
+		web_1 <= web;
+	end if;
+end process;
 	
 end BEH;
