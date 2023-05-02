c918dc834dac30ecfe99524c1fe1c04283f1d21d
zaheer.ahmad <zaheer.ahmad@rapidsilicon.com>
2023-05-02 17:46:26 +0500

    added bitstream testbench for add_1bit design

--- Testcases/add_1bit/sim/bitstream_tb/add_1bit_formal_random_top_tb.v
+++ Testcases/add_1bit/sim/bitstream_tb/add_1bit_formal_random_top_tb.v
@@ -1,184 +1,134 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: FPGA Verilog Testbench for Formal Top-level netlist of Design: add_1bit
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//	Date: Mon Mar 20 11:54:32 2023
-//-------------------------------------------
-//----- Time scale -----
 `timescale 1ns / 1ps
 
-//----- Default net type -----
 `default_nettype none
 
 module add_1bit_top_formal_verification_random_tb;
-// ----- Default clock port is added here since benchmark does not contain one -------
-	reg [0:3] clk;
 
-// ----- Shared inputs -------
-	reg a;
-	reg b;
+	reg  clock0;
+	reg  a;
+	reg  b;
+	wire  c;
 
-// ----- FPGA fabric outputs -------
-	wire c;
-
-// ----- FPGA fabric instanciation -------
-	// add_1bit_top_formal_verification FPGA_DUT(
-	// 	a,
-	// 	b,
-	// 	c_gfpga
-	// );
-// ----- End FPGA Fabric Instanication -------
-
-	wire [0:639] gfpga_pad_QL_PREIO_A2F;
-	wire [0:639] gfpga_pad_QL_PREIO_F2A;
-	wire [0:639] gfpga_pad_QL_PREIO_F2A_DEF0;
-	wire [0:639] gfpga_pad_QL_PREIO_F2A_DEF1;
-	wire [0:639] gfpga_pad_QL_PREIO_F2A_CLK;
+	wire [0:7] clk;
+	wire [0:2303] gfpga_pad_QL_PREIO_A2F;
+	wire [0:2303] gfpga_pad_QL_PREIO_F2A;
+	wire [0:2303] gfpga_pad_QL_PREIO_F2A_CLK;
 	wire [0:9] ccff_head;
 	wire [0:9] ccff_tail;
-	wire test_en;
+	wire global_resetn;
+	wire scan_en;
 	wire scan_mode;
-	wire scan_clk;
-	// bit config_enable;
 	wire prog_clock;
-	// bit [0:0] CFG_DONE;
-	bit global_resetn;
-
-	fpga_top U0_formal_verification (
-		clk,	
-		test_en,
-		scan_mode,
-		scan_clk,
-		prog_clock,
-		global_resetn,
-		gfpga_pad_QL_PREIO_A2F[0:639],
-		gfpga_pad_QL_PREIO_F2A[0:639],
-		gfpga_pad_QL_PREIO_F2A_DEF0[0:639],
-		gfpga_pad_QL_PREIO_F2A_DEF1[0:639],
-		gfpga_pad_QL_PREIO_F2A_CLK[0:639],
-		ccff_head[0:9],
-		ccff_tail[0:9]);
-
+	wire [0:2] rwm;
+	
+		fpga_top U0_formal_verification (
+			clk[0:7],
+			global_resetn[0],
+			scan_en[0],
+			scan_mode[0],
+			prog_clock[0],
+			rwm[0:2],
+			gfpga_pad_QL_PREIO_A2F[0:2303],
+			gfpga_pad_QL_PREIO_F2A[0:2303],
+			gfpga_pad_QL_PREIO_F2A_CLK[0:2303],
+			ccff_head[0:9],
+			ccff_tail[0:9]);
+	
 		assign prog_clock[0] = 1'b0;
-		// assign CFG_DONE[0] = 1'b1;
-		assign test_en[0] = 1'b0;
+		assign global_resetn[0] = 1'b0;
+		assign scan_en[0] = 1'b0;
 		assign scan_mode[0] = 1'b0;
-		assign scan_clk[0] = 1'b0;
-		// assign clk[0] = 1'b0;
-		// assign clk[1] = 1'b0;
-		// assign clk[2] = 1'b0;
-		// assign clk[3] = 1'b0;
-		assign global_resetn = 1'b1;
-		// assign scan_reset[0] = 1'b1;
-
-// ----- Clock 'clk' Initialization -------
-		initial begin
-			clk[0] <= 1'b0;
-			clk[1] <= 1'b0;
-			clk[2] <= 1'b0;
-			clk[3] <= 1'b0;
-			while(1) begin
-				#1
-				clk[0] <= !clk[0];
-				clk[1] <= !clk[1];
-				clk[2] <= !clk[2];
-				clk[3] <= !clk[3];
-			end
+		assign rwm[0:2] = 3'b011;
+		assign clk[0] = clock0;
+		assign clk[1] = 1'b0;
+		assign clk[2] = 1'b0;
+		assign clk[3] = 1'b0;
+		assign clk[4] = 1'b0;
+		assign clk[5] = 1'b0;
+		assign clk[6] = 1'b0;
+		assign clk[7] = 1'b0;
+		
+	initial begin
+		clock0 <= 1'b0;
+		while(1) begin
+			#1
+			clock0 <= !clock0;
 		end
-		`include "../add_1bit/PinMapping.v"
-
-// ----- Begin reset signal generation -----
-// ----- End reset signal generation -----
-
-// ----- Input Initialization -------
-		initial begin
-			`include "../../bitstream_text.txt"
-			// config_enable[0]=1'b1;
-			// CFG_DONE=1'b1;
-			#10;
-			
-			// a <= 1'b0;
-			// b <= 1'b0;
-			// #10
-			a = 1'b0;
-			b = 1'b0;
-			#15
-			if (c === 0)
-			  $display("Status: Test Passed");
-			else  
-			  $display("Status: Test Failed");
-			  
-			a = 1'b0; 
-			b = 1'b1;
-			#15
-			if (c === 1)
-			  $display("Status: Test Passed");
-			else  
-			  $display("Status: Test Failed");
-		  
-			a = 1'b1;
-			b = 1'b0;
-			#15
-			if (c === 1)
-			  $display("Status: Test Passed");
-			else  
-			  $display("Status: Test Failed");
-	  
-			a = 1'b1;
-			b = 1'b1;
-			#15
-			if (c === 0)
-			  $display("Status: Test Passed");
-			else  
-			  $display("Status: Test Failed");
-		  
-			  a = 1'b0;
-			  b = 1'b1;
-			  #15
-			  if (c === 1)
-			  $display("Status: Test Passed");
-			  else  
-			  $display("Status: Test Failed");
-	  
-			  a = 1'b1;
-			  b = 1'b0;
-			  #15
-			  if (c === 1)
-			  $display("Status: Test Passed");
-			  else  
-			  $display("Status: Test Failed");
-	  
-			  a = 1'b1;
-			  b = 1'b1;
-			  #15
-			  if (c === 0)
-				$display("Status: Test Passed");
-			  else  
-				$display("Status: Test Failed");
-			  #10
-			  $finish;
 	end
+	`include "../add_1bit/PinMapping.v"
 
-// ----- Begin output waveform to VCD file-------
+	initial begin
+		`include "../../bitstream_text.txt"
+		// config_enable[0]=1'b1;
+		// CFG_DONE=1'b1;
+		a = 1'b0;
+		b = 1'b0;
+		global_resetn=1'b0;
+		#5;
+		global_resetn=1'b1;
+		#15
+		if (c === 0)
+		  $display("Status: Test Passed");
+		else  
+		  $display("Status: Test Failed");
+		  
+		a = 1'b0; 
+		b = 1'b1;
+		#15
+		if (c === 1)
+		  $display("Status: Test Passed");
+		else  
+		  $display("Status: Test Failed");
+	  
+		a = 1'b1;
+		b = 1'b0;
+		#15
+		if (c === 1)
+		  $display("Status: Test Passed");
+		else  
+		  $display("Status: Test Failed");
+  
+		a = 1'b1;
+		b = 1'b1;
+		#15
+		if (c === 0)
+		  $display("Status: Test Passed");
+		else  
+		  $display("Status: Test Failed");
+	  
+		a = 1'b0;
+		b = 1'b1;
+		#15
+		if (c === 1)
+		  $display("Status: Test Passed");
+		else  
+		  $display("Status: Test Failed");
+
+		a = 1'b1;
+		b = 1'b0;
+		#15
+		if (c === 1)
+		  $display("Status: Test Passed");
+		else  
+		  $display("Status: Test Failed");
+
+		a = 1'b1;
+		b = 1'b1;
+		#15
+		if (c === 0)
+		  $display("Status: Test Passed");
+		else  
+		  $display("Status: Test Failed");
+		#10
+		$finish;
+end
+
 	initial begin
 		$dumpfile("add_1bit_formal.vcd");
 		$dumpvars(1, add_1bit_top_formal_verification_random_tb);
 	end
-// ----- END output waveform to VCD file -------
-
-// initial begin
-// 	$timeformat(-9, 2, "ns", 20);
-// 	$display("Simulation start");
-// // ----- Can be changed by the user for his/her need -------
-// 	#20
-// 	$display("Simulation Succeed");
-// 	$finish;
-// end
 
 endmodule
-// ----- END Verilog module for add_1bit_top_formal_verification_random_tb -----
 
-//----- Default net type -----
 `default_nettype none
 
