562898444e05b21a5313fa56809b1d6c12fae6e8
zaheer.ahmad <zaheer.ahmad@rapidsilicon.com>
2023-05-10 17:38:50 +0500

    updated the shift_reg testcase and testbench

--- Testcases/shift_register/sim/post_route_tb/sim_route_shift_register.sv
+++ Testcases/shift_register/sim/post_route_tb/sim_route_shift_register.sv
@@ -13,7 +13,27 @@ module sim_route_shift_register;
   integer mismatch=0;
 
   shift_register #(.N(N)) shift_register_dut (.clk (clk),.reset(reset),.ctrl(ctrl),.data(data),.q_reg(q_reg_rtl));
-  shift_register_post_route netlist (clk,reset,data[7],data[6],data[5],data[4],data[3],data[2],data[1],data[0],ctrl[1],ctrl[0],q_reg[7],q_reg[6],q_reg[5],q_reg[4],q_reg[3],q_reg[2],q_reg[1],q_reg[0]);
+  shift_register_post_route netlist (clk,
+                                    reset,
+                                    data[7],
+                                    data[6],
+                                    data[5],
+                                    data[4],
+                                    data[3],
+                                    data[2],
+                                    data[1],
+                                    data[0],
+                                    ctrl[1],
+                                    ctrl[0],
+                                    q_reg[7],
+                                    q_reg[6],
+                                    q_reg[5],
+                                    q_reg[4],
+                                    q_reg[3],
+                                    q_reg[2],
+                                    q_reg[1],
+                                    q_reg[0]
+                                    );
 
   always #5  clk = !clk;
      
