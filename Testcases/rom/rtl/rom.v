bcd7146937a3961c3ef3ed392a865f36e755e777
zaheer.ahmad <zaheer.ahmad@rapidsilicon.com>
2023-05-16 15:56:38 +0500

    updated the post route testbench of rom

--- Testcases/rom/rtl/rom.v
+++ Testcases/rom/rtl/rom.v
@@ -6,7 +6,7 @@ output reg [7:0] data
 
 reg [7:0] mem [1023:0]; 
 
-initial $readmemb("memory_file.mem", mem); 
+initial $readmemb("MEM_FILE_PATH/memory_file.mem", mem); 
 
 always @(posedge clk) begin
    data <= mem[address];
