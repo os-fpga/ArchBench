370d016b5a583e70ea08ee1b3af4426c1285068f
zaheer.ahmad <zaheer.ahmad@rapidsilicon.com>
2023-05-16 15:56:38 +0500

    updated the post route testbench of rom

--- Testcases/rom/sim/post_route_tb/sim_route_rom.sv
+++ Testcases/rom/sim/post_route_tb/sim_route_rom.sv
@@ -6,11 +6,27 @@ module rom_post_route_tb;
   integer mismatch=0;
   integer i=0;
   
-  rom golden (clk,address,data);
-  rom_post_route netlist (clk,address[2],address[1],address[0],data_netlist[6],data_netlist[5],data_netlist[4],data_netlist[3],data_netlist[2],data_netlist[7],data_netlist[1],data_netlist[0]);
+  rom golden (.clk(clk),.address(address),.data(data));
+  rom_post_route netlist (clk,
+                          address[7],
+                          address[6],
+                          address[5],
+                          address[4],
+                          address[3],
+                          address[2],
+                          address[1],
+                          address[0],
+                          data_netlist[7],
+                          data_netlist[6],
+                          data_netlist[5],
+                          data_netlist[4],
+                          data_netlist[3],
+                          data_netlist[2],
+                          data_netlist[1],
+                          data_netlist[0]
+                          );
 
-  always
-  #1  clk = !clk;
+  always #1 clk = !clk;
   
   initial begin
     for (i = 0; i <1023; i = i+1 )begin
