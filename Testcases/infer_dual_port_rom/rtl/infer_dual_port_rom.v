909f888158a600a730860a5e47f71c223eb45764
zaheer.ahmad <zaheer.ahmad@rapidsilicon.com>
2023-05-19 15:46:59 +0500

    added dual_port_rom design

---
+++ Testcases/infer_dual_port_rom/rtl/infer_dual_port_rom.v
@@ -0,0 +1,21 @@
+module infer_dual_port_rom
+#(parameter DATA_WIDTH=12, parameter ADDR_WIDTH=10)
+(
+	input [(ADDR_WIDTH-1):0] addr_a, addr_b,
+	input clk, 
+	output reg [(DATA_WIDTH-1):0] q_a, q_b
+);
+
+	// Declare the ROM variable
+	reg [DATA_WIDTH-1:0] rom[2**ADDR_WIDTH-1:0];
+
+	initial $readmemh("/nfs_scratch/scratch/CompilerValidation/zaheer_ahmad/bitstream_simulation_new/test/ArchBench/Testcases/infer_dual_port_rom/rtl/memory_test.mem",rom);  //reg [DATA_WIDTH-1:0] rom[2**ADDR_WIDTH-1:0];
+
+	always @ (posedge clk)
+	begin
+		q_a <= rom[addr_a];
+		q_b <= rom[addr_b];
+	end
+
+endmodule
+
