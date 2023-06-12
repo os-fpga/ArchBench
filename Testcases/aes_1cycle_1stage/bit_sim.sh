3359c7e6de7b297a2f2337bf2455941e1ec30a69
zaheerahmad-rs <zaheer.ahmad@rapidsilicon.com>
2023-06-12 14:25:17 +0500

    added option of device gemini_compact_104x8 in script

--- Testcases/aes_1cycle_1stage/bit_sim.sh
+++ Testcases/aes_1cycle_1stage/bit_sim.sh
@@ -5,7 +5,7 @@ main_path=$PWD
 
 design_name=${PWD##*/}
 
-device="GEMINI_COMPACT_82x68"
+device="GEMINI_COMPACT_104x68"
 
 xml_root=`git rev-parse --show-toplevel`
 cd $xml_root/openfpga-pd-castor-rs 
@@ -26,6 +26,8 @@ if [[ $# -eq 1 ]]; then
         source $main_path/../tool_62x44.conf
     elif [ "$device" == "GEMINI_COMPACT_82x68" ]; then
         source $main_path/../tool_82x68.conf
+    elif [ "$device" == "GEMINI_COMPACT_104x68" ]; then
+        source $main_path/../tool_104x68.conf
     else
         source $main_path/../../scripts/empty.conf
     fi
