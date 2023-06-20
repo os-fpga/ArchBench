89fd0d7c099c8ca75d42e9400b5b2d657d0d806c
zaheerahmad-rs <zaheer.ahmad@rapidsilicon.com>
2023-06-20 17:25:05 +0500

    updated the mem file and its path in rom

--- Testcases/rom/rtl/rom.v
+++ Testcases/rom/rtl/rom.v
@@ -6,7 +6,7 @@ output reg [7:0] data
 
 reg [7:0] mem [1023:0]; 
 
-initial $readmemb("/nfs_scratch/scratch/CompilerValidation/zaheer_ahmad/bitstream_simulation_new/ArchBench/Testcases/rom/rtl/memory_file.mem", mem); 
+initial $readmemb("MEM_FILE_PATH/memory_file.mem", mem); 
 
 always @(posedge clk) begin
    data <= mem[address];
