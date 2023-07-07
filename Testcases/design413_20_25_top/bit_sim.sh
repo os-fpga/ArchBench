4827dcf6643ed727bd726e372090ea0c23f76385
zaheerahmad-rs <zaheer.ahmad@rapidsilicon.com>
2023-07-07 10:40:28 +0500

    updated the device for design413_20_25_top

--- Testcases/design413_20_25_top/bit_sim.sh
+++ Testcases/design413_20_25_top/bit_sim.sh
@@ -86,7 +86,7 @@ library=${raptor_path/$lib_fix_path//share/raptor/sim_models/rapidsilicon}
 cd $design_name\_golden
 
 echo "create_design $design_name">raptor.tcl
-echo "target_device GEMINI_COMPACT_104x68">>raptor.tcl
+echo "target_device $device">>raptor.tcl
 [ -z "$vpr_file_path" ] || [ -z "$openfpga_file_path" ] && echo "">>raptor.tcl || echo "architecture $vpr_file_path $openfpga_file_path">>raptor.tcl
 echo "add_include_path ../rtl">>raptor.tcl
 echo "add_library_path ../rtl">>raptor.tcl  
