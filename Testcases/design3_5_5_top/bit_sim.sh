250b71a2f5571edf97d7faf3eec707077cb28a94
zaheerahmad-rs <zaheer.ahmad@rapidsilicon.com>
2023-08-09 13:03:05 +0500

    added pin_assign_method free in placement failing designs scripts

--- Testcases/design3_5_5_top/bit_sim.sh
+++ Testcases/design3_5_5_top/bit_sim.sh
@@ -99,6 +99,7 @@ echo "set_top_module $design_name">>raptor.tcl
 [ -z "$bitstream_setting_path" ] || [ -z "$fixed_sim_openfpga_path" ] || [ -z "$repack_design_constraint_path" ] && echo "" || echo "bitstream_config_files -bitstream $bitstream_setting_path -sim $fixed_sim_openfpga_path -repack $repack_design_constraint_path">>raptor.tcl
 [ -z "$set_channel_width" ] && echo "" || echo "set_channel_width $set_channel_width">>raptor.tcl
 echo "add_constraint_file ../clk_constraint.sdc">>raptor.tcl 
+echo "pin_loc_assign_method free">>raptor.tcl
 echo "synthesize $strategy">>raptor.tcl
 echo "packing">>raptor.tcl  
 echo "global_placement">>raptor.tcl  
