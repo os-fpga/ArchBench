b5ed83ab8e7bdf6240361fba0ca4ff90a28acb3e
zaheerahmad-rs <zaheer.ahmad@rapidsilicon.com>
2023-08-28 17:56:25 +0500

    updated bitstream simulation testbench for up5bit_counter design

--- Testcases/up5bit_counter/sim/bitstream_tb/up5bit_counter_formal_random_top_tb.v
+++ Testcases/up5bit_counter/sim/bitstream_tb/up5bit_counter_formal_random_top_tb.v
@@ -19,69 +19,56 @@ module up5bit_counter_top_formal_verification_random_tb;
 	reg reset;
 
 // ----- FPGA fabric outputs -------
-	wire [0:4] out;
+	wire [4:0] out;
 	integer i;
-	reg [6:0] counter_model=0;
+	reg [4:0] counter_model=5'b00001;
 	reg[31:0] error=0;
-	// wire out[3];
-	// wire out[2];
-	// wire out[1];
-	// wire out[0];
 
-// ----- FPGA fabric instanciation -------
-	// up5bit_counter_top_formal_verification FPGA_DUT(
-	// 	clk,
-	// 	reset,
-	// 	out[4],
-	// 	out[3],
-	// 	out[2],
-	// 	out[1],
-	// 	out[0]
-	// );
-// ----- End FPGA Fabric Instanication -------
-
-	wire [0:7] clk;
+	wire [0:15] clk;
 	wire [0:2303] gfpga_pad_QL_PREIO_A2F;
 	wire [0:2303] gfpga_pad_QL_PREIO_F2A;
 	wire [0:2303] gfpga_pad_QL_PREIO_F2A_CLK;
-	wire [0:9] ccff_head;
-	wire [0:9] ccff_tail;
+	wire [0:513] bl_config_region_0;
+	wire [0:406] wl_config_region_0;
 	bit global_resetn;
 	wire scan_en;
 	wire scan_mode;
-	wire prog_clock;
-	wire [0:2] rwm;
-	
-	// ----- FPGA top-level module to be capsulated -----
-		fpga_top U0_formal_verification (
-			.clk(clk[0:7]),
-			.global_resetn(global_resetn),
-			.scan_en(scan_en),
-			.scan_mode(scan_mode),
-			.prog_clock(prog_clock),
-			.rwm(rwm[0:2]),
-			.gfpga_pad_QL_PREIO_A2F(gfpga_pad_QL_PREIO_A2F[0:2303]),
-			.gfpga_pad_QL_PREIO_F2A(gfpga_pad_QL_PREIO_F2A[0:2303]),
-			.gfpga_pad_QL_PREIO_F2A_CLK(gfpga_pad_QL_PREIO_F2A_CLK[0:2303]),
-			.ccff_head(ccff_head[0:9]),
-			.ccff_tail(ccff_tail[0:9]));
-	
-	// ----- Begin Connect Global ports of FPGA top module -----
-		assign prog_clock = 1'b0;
-		// assign global_resetn = 1'b0;
-		assign scan_en = 1'b0;
-		assign scan_mode = 1'b0;
-		assign rwm[0:2] = 3'b011;
-		// assign rwm[1] = 1'b0;
-		// assign rwm[2] = 1'b0;
-		assign clk[0] = clock0;
-		assign clk[1] = 1'b0;
-		assign clk[2] = 1'b0;
-		assign clk[3] = 1'b0;
-		assign clk[4] = 1'b0;
-		assign clk[5] = 1'b0;
-		assign clk[6] = 1'b0;
-		assign clk[7] = 1'b0;
+
+	fpga_top U0_formal_verification (
+		.clk(clk[0:15]),
+		.global_resetn(global_resetn),
+		.scan_en(scan_en),
+		.scan_mode(scan_mode),
+		.gfpga_pad_QL_PREIO_A2F(gfpga_pad_QL_PREIO_A2F[0:2303]),
+		.gfpga_pad_QL_PREIO_F2A(gfpga_pad_QL_PREIO_F2A[0:2303]),
+		.gfpga_pad_QL_PREIO_F2A_CLK(gfpga_pad_QL_PREIO_F2A_CLK[0:2303]),
+		.bl_config_region_0(bl_config_region_0[0:513]),
+		.wl_config_region_0(wl_config_region_0[0:406]));
+
+	assign gfpga_pad_QL_PREIO_A2F [0:71] = 0;
+	assign gfpga_pad_QL_PREIO_A2F [73:190] = 0;
+	assign gfpga_pad_QL_PREIO_A2F [192:2303] = 0;
+	assign bl_config_region_0 [0:513] = 0;
+	assign wl_config_region_0 [0:406] = 0;
+	// assign global_resetn = 1'b0;
+	assign scan_en = 1'b0;
+	assign scan_mode = 1'b0;
+	assign clk[0] = clock0;
+	assign clk[1] = clock0;
+	assign clk[2] = clock0;
+	assign clk[3] = clock0;
+	assign clk[4] = clock0;
+	assign clk[5] = clock0;
+	assign clk[6] = clock0;
+	assign clk[7] = clock0;
+	assign clk[8] = clock0;
+	assign clk[9] = clock0;
+	assign clk[10] = clock0;
+	assign clk[11] = clock0;
+	assign clk[12] = clock0;
+	assign clk[13] = clock0;
+	assign clk[14] = clock0;
+	assign clk[15] = clock0;
 // ----- Clock 'clk' Initialization -------
 	initial begin
 		clock0 <= 1'b0;
@@ -99,12 +86,12 @@ module up5bit_counter_top_formal_verification_random_tb;
 
 	initial begin
 	`include "../../bitstream_text.txt"
-		reset <= 1'b1;
-		global_resetn <= 1'b0;
+		reset = 1'b1;
+		global_resetn = 1'b0;
 		repeat(2)@(posedge clock0);
-		reset <= 1'b0;
+		reset = 1'b0;
 		repeat(8)@(posedge clock0);
-		global_resetn <= 1'b1;
+		global_resetn = 1'b1;
 
 		
 		for (i=0; i<2**4; i=i+1)begin
@@ -128,8 +115,8 @@ module up5bit_counter_top_formal_verification_random_tb;
 
 // ----- Begin output waveform to VCD file-------
 	initial begin
-		$dumpfile("up5bit_counter_formal.vcd");
-		$dumpvars(1, up5bit_counter_top_formal_verification_random_tb);
+		$dumpfile("up5bit_counter.vcd");
+		$dumpvars(0, up5bit_counter_top_formal_verification_random_tb);
 	end
 // ----- END output waveform to VCD file -------
 
