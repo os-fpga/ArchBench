3e3d27e12b2ce5e144152249502235e182b9f382
zaheerahmad-rs <zaheer.ahmad@rapidsilicon.com>
2024-01-13 23:12:49 +0500

    added library_ext in tcl of vexrisc testcases

--- Testcases/vexriscv_axi_ram/bit_sim.sh
+++ Testcases/vexriscv_axi_ram/bit_sim.sh
@@ -90,6 +90,7 @@ echo "target_device $device">>raptor.tcl
 [ -z "$vpr_file_path" ] || [ -z "$openfpga_file_path" ] && echo "" || echo "architecture $vpr_file_path $openfpga_file_path">>raptor.tcl
 echo "add_include_path ../rtl">>raptor.tcl
 echo "add_library_path ../rtl">>raptor.tcl  
+echo "add_library_ext .v .sv">>raptor.tcl 
 echo "add_design_file -V_2001 ../rtl/vex_soc.v">>raptor.tcl
 echo "set_top_module vex_soc">>raptor.tcl
 [ -z "$set_device_size" ] && echo "" || echo "set_device_size $set_device_size">>raptor.tcl
