1afe59f9fc2e1fa011edbe265e40d99d60b57fb2
zhergarvi <zhergarvi133@gmail.com>
2024-01-26 19:11:26 +0500

    added a pin_assignment script

--- Testcases/unsigned_multiply/bit_sim.sh
+++ Testcases/unsigned_multiply/bit_sim.sh
@@ -107,7 +107,7 @@ echo "place">>raptor.tcl
 echo "route">>raptor.tcl  
 echo "sta">>raptor.tcl  
 echo "power">>raptor.tcl
-[ -z "$vpr_file_path" ] && echo "bitstream">>raptor.tcl || echo "bitstream">>raptor.tcl    # enable_simulation
+[ -z "$vpr_file_path" ] && echo "bitstream">>raptor.tcl || echo "bitstream write_xml pb_pin_fixup">>raptor.tcl    # enable_simulation
 
 xml_version=`cd $xml_root/openfpga-pd-castor-rs && git describe --tags --abbrev=0`
 
@@ -212,6 +212,7 @@ fi
 cd $design_name/$design_name\_golden/$design_name\_$tool_name\_bitstream_sim_files
 
 python3 ../../../../scripts/force.py $design_name
+python3 ../../../../scripts/pin_assignment.py $design_name
 
 start_bitstream=`date +%s`
 # timeout 20m vcs -sverilog $bitstream_tb_path -full64 -debug_all -lca -kdb | tee bitstream_sim.log
