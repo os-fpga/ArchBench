cf94c2c3222923712bcf9bcd8b9fd3a29851a831
zhergarvi <zhergarvi133@gmail.com>
2024-02-21 16:01:51 +0500

    added export TMPDIR flag in testcases

--- Testcases/add_1bit_1GE100_ES1/sim/bitstream_tb/add_1bit_include_netlists.v
+++
@@ -1,7 +0,0 @@
-
-`timescale 1ns / 1ps
-
-`include "../../../SRC/fabric_netlists.v"
-
-// `include "BIT_SIM/add_1bit_top_formal_verification.v"
-`include "../../sim/bitstream_tb/add_1bit_formal_random_top_tb.v"
