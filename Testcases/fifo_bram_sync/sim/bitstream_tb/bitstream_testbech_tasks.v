ec50ea8de7b6f8ed46f0f0f0ad829ffb3cb202d0
zhergarvi <zhergarvi133@gmail.com>
2024-07-11 17:29:29 +0500

    fix for bit sim failures

--- Testcases/fifo_bram_sync/sim/bitstream_tb/bitstream_testbech_tasks.v
+++ Testcases/fifo_bram_sync/sim/bitstream_tb/bitstream_testbech_tasks.v
@@ -7,7 +7,7 @@
         @(negedge clock0);
         write = 0;
         exp_q.push_back(write_data);
-        $display ("PUSH DATA:%0h FULL:%0b EMPTY:%0b", write_data, full_reg_gfpga, empty_reg_gfpga);
+        $display ("PUSH DATA:%0h FULL:%0b EMPTY:%0b", write_data, full_gfpga, empty_gfpga);
         endtask
     //pop
     task pop ();
@@ -21,5 +21,5 @@
             error+=1; 
             $display("Status: Test Failed:: MISMATCH! PUSH DATA:%0h, POP DATA:%0h",q_rdata, read_data_gfpga);
         end
-        else $display ("Status: Test Passed:: POP DATA:%0h FULL:%0b EMPTY:%0b", read_data_gfpga, full_reg_gfpga, empty_reg_gfpga);
+        else $display ("Status: Test Passed:: POP DATA:%0h FULL:%0b EMPTY:%0b", read_data_gfpga, full_gfpga, empty_gfpga);
     endtask
